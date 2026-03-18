a91131febd812bd6a6854dc8d6aa92a89c047bce
Rodrigo Ferreira <rodrigonito3@gmail.com>
2026-03-17 22:54:22 -0300

    logout

--- app/Http/Controllers/AuthUserController.php
+++ app/Http/Controllers/AuthUserController.php
@@ -71,15 +71,15 @@ class AuthUserController extends Controller
     public function perfil(Request $request)
     {
         try {
-    
+
             $user = $request->user();
             $psicologo = Psicologo::where('id_usuario', $user->id_usuario)->first();
-    
+
             return response()->json([
                 'user' => $user,
                 'psicologo' => $psicologo
             ], 200);
-    
+
         } catch (\Exception $e) {
             return response()->json([
                 'error' => 'Erro ao obter perfil',
